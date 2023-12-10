afeda9faa3408ca494138824bcd65d7aafc96c4e
XzerozZ <111548296+XzerozZ@users.noreply.github.com>
2023-12-11 04:40:58 +0700

    ad 2

--- src/pages/market.jsx
+++ src/pages/market.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import './market.css';
 import Nav from "../components/NAVX";
-import Cyberpunk from "../assets/Cyberpunk.gif";
-import Cyberpunk_logo from '../assets/cyberpunk0.png';
-import { Button } from '../components/Button';
+import Ad from '../components/Ad';
 export const MarketPages = () => {
   return (
   <>
-  <div className='Advertise'>
-        <img  className="item-show" src={Cyberpunk} alt="" />
-        <Nav/>
-        <div class='z-1' className='buy-now'>
-          <img className='cyber_logo' src={Cyberpunk_logo} alt='' />
-          <Button/>
-        </div>
-        
-  </div>
+  <Nav/>
+  <Ad/>
   </>
   )
 }
