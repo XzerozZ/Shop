f6a04e3e91a9321a1091f4b0f4495b63a92cf17c
Narutchai <koonfluk2015@gmail.com>
2023-12-21 02:48:24 +0700

    update showtransaction

--- Server/src/data/showtransaction.ts
+++ Server/src/data/showtransaction.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from 'express';
 import { client , Database } from "../server";
+import { ObjectId } from 'mongodb';
 
 export const showtrans = async (req: Request, res: Response) => {
     try {
         await Database();
         const { userID } = req.params
-        const result = await client.db("Webpro").collection('Transaction').find({ userId: userID }).toArray();
-        console.log('Fetched data:', result);
+        const result = await client.db("Webpro").collection("Transaction").aggregate([
+            {
+                $match: {
+                    userID: userID
+                }
+            },
+            {
+                $lookup: {
+                    from: "product",
+                    localField: "productID",
+                    foreignField: "_id",
+                    as: "game"
+                }
+            },
+           
+        ]).toArray();
         res.status(200).send(result);
     }catch{
         res.status(500).send({ error: "Internal Server Error" });
